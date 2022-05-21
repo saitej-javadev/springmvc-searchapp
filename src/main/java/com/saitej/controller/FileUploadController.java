5cb460dfefcb5a2456ce35d91bfae46a123378fe
saitej <saitej.galipelli@gmail.com>
2022-05-21 23:52:42 +0530

    fileupload implementation implemented

--- src/main/java/com/saitej/controller/FileUploadController.java
+++ src/main/java/com/saitej/controller/FileUploadController.java
@@ -1,12 +1,16 @@
 package com.saitej.controller;
 
-import com.saitej.model.Customer;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 @Controller
 public class FileUploadController {
@@ -18,14 +22,27 @@ public class FileUploadController {
     }
 
     @PostMapping("/uploadFile")
-    public ModelAndView fileUpload(@RequestParam("profile") CommonsMultipartFile file){
+    public ModelAndView fileUpload(@RequestParam("profile") CommonsMultipartFile file,
+                                   HttpSession session) throws IOException {
 
         System.out.println(file.getSize());
         System.out.println(file.getContentType());
         System.out.println(file.getName());
         System.out.println(file.getOriginalFilename());
         System.out.println(file.getStorageDescription());
+        byte[] data = file.getBytes();
+
+        String path = session.getServletContext().getRealPath("/")+"WEB-INF"+File.separator
+                +"resources"+File.separator
+                +"images"+File.separator+file.getOriginalFilename();
+
+        System.out.println(path);
+        FileOutputStream fos = new FileOutputStream(path);
+        fos.write(data);
+        fos.close();
+        System.out.println("file uploaded");
         ModelAndView mav = new ModelAndView();
+        mav.addObject("filename",file.getOriginalFilename());
         mav.setViewName("filesuccess");
         return mav;
 
--- src/main/webapp/WEB-INF/views/filesuccess.jsp
+++ src/main/webapp/WEB-INF/views/filesuccess.jsp
@@ -1,16 +1,11 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: abhinay
-  Date: 5/21/2022
-  Time: 4:47 PM
-  To change this template use File | Settings | File Templates.
---%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<%@ page contentType="text/html;charset=UTF-8" language="java"
+isELIgnored="false" %>
 <html>
 <head>
     <title>File Success Page</title>
 </head>
 <body>
-<h1>File upload success!!!</h1>
+<h1>${filename} uploaded Successfully......</h1>
 </body>
 </html>
